4d1ca4cff6504f7034635bdbf07739e8ee073e4c
Adam Grobman <25434173+acgrobman@users.noreply.github.com>
2019-11-03 16:45:14 -0800

    Basic roster list added
    Does not yet have actual on-press functionality to display student data.

--- pages/details_subscreens/RosterScreen.js
+++ pages/details_subscreens/RosterScreen.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 
 export default class RosterScreen extends Component {
-    render () {
+  renderItem = ({ index }) => {
+    return (
+      <View style={{ height: 50 }}>
+        <TouchableOpacity onPress={() => alert('Details of students nyi')}>
+          <Text>Item {index}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+  
+  render () {
+    const data = new Array(150).fill("Test Student");
+    //TODO: data needs to be somehow gotten to be the class lists
         return (
             <View
             style={{
               backgroundColor: 'green',
               flex: 1,
-              justifyContent: 'center',
+              justifyContent: 'space-between',
               alignItems: 'center',
             }}>
-            <Text style={{ color: 'white', fontSize: 30 }}>
-                Roster {JSON.stringify(this.props.navigation.getParam('course', 'default course'))}
-            </Text>
+            {/* Can get course id that was passed in though {JSON.stringify(this.props.navigation.getParam('course', 'default course'))}*/}
+            <Text style={{ color: 'white', fontSize: 18 }}>Roster</Text>
+            <View>
+              {(data && data.constructor === Array && data.length === 0) ? <Text> Add a roster to get started</Text> : <Text></Text>}
+              <FlatList
+                data={data}
+                renderItem={this.renderItem}
+                contentContainerStyle={{flex: 1}}
+                />
+            </View>
           </View>
         );
     }

4e2adf031c9bb3ff88d51a87734e05b19dc3ba22
Adam Grobman <25434173+acgrobman@users.noreply.github.com>
2019-11-09 13:33:14 -0800

    Implement ViewPager for graphs

--- pages/details_subscreens/GraphsScreen.js
+++ pages/details_subscreens/GraphsScreen.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { IndicatorViewPager, PagerDotIndicator } from 'rn-viewpager';
+import Graph1 from './graphs/Graph1';
+import Graph2 from './graphs/Graph2';
+import Graph3 from './graphs/Graph3';
 
-export default class GraphsScreen extends Component {
-    render () {
-        return (
-            <View
-            style={{
-              backgroundColor: 'blue',
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text style={{ color: 'white', fontSize: 30 }}>
-                Graphs {JSON.stringify(this.props.navigation.getParam('course', 'default course'))}
-            </Text>
-          </View>
-        );
-    }
+/** Screen component for page containing all graphs */
+export default class GraphScreen extends Component {
+  
+  /** Renders the page */
+  render() {
+    return (
+      <View style={{ flex: 1}}>
+        <IndicatorViewPager
+          style={{ flex: 1 }}
+          indicator={this._renderDotIndicator()}>
+          {/* All ViewPager children must be wrapped in Views 
+          even if they themselves return a view */}
+          <View><Graph1 /></View>
+          <View><Graph2 /></View>
+          <View><Graph3 /></View>
+        </IndicatorViewPager>
+      </View>
+    );
+  }
+
+  /** Render the ViewPager indicator */
+  _renderDotIndicator() {
+    return <PagerDotIndicator pageCount={3} />;
+  }
 }

b45a61e938a5c8559afe03053a29659cd11e60a4
Adam Grobman <25434173+acgrobman@users.noreply.github.com>
2019-11-09 14:48:56 -0800

    Working swipe functionality

--- pages/details_subscreens/RemediationScreen.js
+++ pages/details_subscreens/RemediationScreen.js
@@ -1,46 +1,169 @@
+// Based on example code from 
+//https://github.com/lhandel/react-native-card-stack-swiper/blob/master/Example/App.js
+
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
-import CardStack, { Card } from 'react-native-card-stack-swiper'
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import CardStack, { Card } from 'react-native-card-stack-swiper';
+import { responsiveHeight, responsiveWidth} from 'react-native-responsive-dimensions';
+
+/** Screen for providing feedback to the facial recognition algorithm */
+export default class RemediationScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <CardStack
+          style={styles.content}
+          renderNoMoreCards={this._outOfCards}
+          ref={swiper => {
+            this.swiper = swiper
+          }}
+          onSwipedLeft={() => alert("Incorrect match")}
+        >
+          <Card style={[styles.card, styles.card1]}>
+            <Text style={styles.label}>A</Text>
+          </Card>
+          <Card style={[styles.card, styles.card2]}>
+            <Text style={styles.label}>B</Text>
+          </Card>
+          <Card style={[styles.card, styles.card1]}>
+            <Text style={styles.label}>C</Text>
+          </Card>
+          <Card style={[styles.card, styles.card2]}>
+            <Text style={styles.label}>D</Text>
+          </Card>
+          <Card style={[styles.card, styles.card1]}>
+            <Text style={styles.label}>E</Text>
+          </Card>
+        </CardStack>
+  
+        {this._renderFooter()}
+        </View>
+    
+    );
+  }
+
+  _renderFooter() {
+    return(
+      <View style={styles.footer}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity 
+            style={[styles.button, styles.red]}
+            onPress={() => {this.swiper.swipeLeft();}}>
+              <Image 
+                source={require('./assets/red.png')}
+                resizeMode={'contain'}
+                style={{ height: 62, width: 62 }} />
+          </TouchableOpacity>
+          <TouchableOpacity 
+            style={[styles.button, styles.orange]} 
+            onPress={() => {this.swiper.goBackFromLeft();}}>
+              <Image 
+              source={require('./assets/back.png')} 
+              resizeMode={'contain'} 
+              style={{ height: 32, width: 32, borderRadius: 5 }} />
+          </TouchableOpacity>
+          <TouchableOpacity 
+            style={[styles.button, styles.green]}
+            onPress={() => {this.swiper.swipeRight();}}>
+              <Image 
+                source={require('./assets/green.png')} 
+                resizeMode={'contain'} 
+                style={{ height: 62, width: 62 }} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
+  _outOfCards() {
+    return (
+    <Text style={{ fontWeight: '700', fontSize: 18, color: 'gray' }}>
+      No more unmatched students! 🎉
+    </Text>);
+  }
+};
 
 const styles = StyleSheet.create({
-  card: {
-    backgroundColor: 'blue',
-    width: Dimensions.get('window').width - 4,
-    height: Dimensions.get('window').width - 4,
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#f2f2f2',
   },
-  label:{
-
+  content:{
+    flex: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  content: {
-    backgroundColor: 'green',
+  card:{
+    height: responsiveHeight(50),
+    width: responsiveWidth(95),
+    backgroundColor: '#FE474C',
+    borderRadius: 5,
+    shadowColor: 'rgba(0,0,0,0.5)',
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    shadowOpacity:0.5,
+  },
+  card1: {
+    backgroundColor: '#FE474C',
+  },
+  card2: {
+    backgroundColor: '#FEB12C',
+  },
+  label: {
+    lineHeight: 400,
+    textAlign: 'center',
+    fontSize: 55,
+    fontFamily: 'System',
+    color: '#ffffff',
+    backgroundColor: 'transparent',
+  },
+  footer:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  buttonContainer:{
+    width:220,
+    flexDirection:'row',
+    justifyContent: 'space-between',
+  },
+  button:{
+    shadowColor: 'rgba(0,0,0,0.3)',
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    shadowOpacity:0.5,
+    backgroundColor:'#fff',
+    alignItems:'center',
+    justifyContent:'center',
+    zIndex: 0,
+  },
+  orange:{
+    width:55,
+    height:55,
+    borderWidth:6,
+    borderColor:'rgb(246,190,66)',
+    borderRadius:55,
+    marginTop:-15
+  },
+  green:{
+    width:75,
+    height:75,
+    backgroundColor:'#fff',
+    borderRadius:75,
+    borderWidth:6,
+    borderColor:'#01df8a',
+  },
+  red:{
+    width:75,
+    height:75,
+    backgroundColor:'#fff',
+    borderRadius:75,
+    borderWidth:6,
+    borderColor:'#fd267d',
   }
-}) 
-
-export default class RemediationScreen extends Component {
-  render () {
-        return (
-            <View
-            style={{
-              backgroundColor: 'yellow',
-              flex: 1,
-            }}>
-              <CardStack 
-                style={styles.content} 
-                disableTopSwipe={true}
-                disableBottomSwipe={true}
-                ref={swiper => { this.swiper = swiper }}>
-                  <Card style={[styles.card]}>
-                    <Text style={styles.label}>A</Text>
-                  </Card>
-                  <Card style={[styles.card]}>
-                    <Text style={styles.label}>B</Text>
-                  </Card>
-                  <Card style={[styles.card]}>
-                    <Text style={styles.label}>C</Text>
-                  </Card>
-                </CardStack>
-          </View>
-        );
-    }
-
-}
+});

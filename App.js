485f3a43c42bb2d5510c5ad56936c724ebbc1bbb
Adam Grobman <25434173+acgrobman@users.noreply.github.com>
2019-11-10 21:34:05 -0800

    Include attempts at customizing login

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from "react"
 import { StyleSheet, YellowBox } from "react-native"
 import Amplify from "aws-amplify"
-import { withAuthenticator } from "aws-amplify-react-native"
+import { withAuthenticator, AmplifyTheme } from "aws-amplify-react-native"
 import Navigator from "./Navigator"
 import config from "./aws-exports"
+
+const MyButton = Object.assign({}, AmplifyTheme.button, { backgroundColor: '#65bf90' });
+const MyTheme = Object.assign({}, AmplifyTheme, { button: MyButton });
 
 Amplify.configure(config);
 
@@ -28,3 +31,24 @@ const styles = StyleSheet.create({
 YellowBox.ignoreWarnings(['Unhandled Promise']);
 
 export default withAuthenticator(App, false);
+
+//Does neither
+/*export default withAuthenticator(App, {
+	includeGreetings: false,
+	authenticatorComponents: [],
+	federated: null,
+	theme: {MyTheme},
+	signUpConfig: {usernameAttributes: 'email' }
+});*/
+	
+//Does theme
+//export default withAuthenticator(App, false,[],null,MyTheme, {usernameAttributes: 'email' });
+
+//Does theme
+//export default withAuthenticator(App, false, [], null, MyTheme);
+
+//Does username
+//export default withAuthenticator(App, {usernameAttributes: 'email' })
+
+// Does username
+//export default withAuthenticator(App, {usernameAttributes: 'email'}, MyTheme);

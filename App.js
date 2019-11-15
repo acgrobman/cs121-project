0003cd9551bb5334365f0eab037cdfebc7cda368
Beau Taylor-Ladd <beautaylorladd@gmail.com>
2019-11-15 11:23:49 -0800

    Quick stash of some styled components

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from "react"
+import { ThemeProvider } from 'styled-components'
 import { StyleSheet, YellowBox } from "react-native"
 import Amplify from "aws-amplify"
 import { withAuthenticator } from "aws-amplify-react-native"
 import Navigator from "./Navigator"
 import config from "./aws-exports"
+import theme from "./assets/theme"
 
 Amplify.configure(config);
 
 class App extends React.Component {
 	render() {
 		return (
-			<Navigator />
+			<ThemeProvider theme={theme}>
+				<Navigator />
+			</ThemeProvider>
 		);
 	}
 }
---
+++ assets/theme.js
@@ -0,0 +1,7 @@
+export default theme = {
+    primary: "#000",
+    secondary: "#1b1b1b",
+    tertiary: "#ccc",
+    green: "#2dd881",
+    blue: "34e4ea"
+}

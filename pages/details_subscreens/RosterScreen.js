b307d4bef62ba512dbd8348744d98e32d54cc485
Adam Grobman <25434173+acgrobman@users.noreply.github.com>
2019-11-17 15:44:38 -0800

    Add basic new student flow

--- pages/details_subscreens/RosterScreen.js
+++ pages/details_subscreens/RosterScreen.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text} from 'react-native';
+import { View} from 'react-native';
 import { FlatList } from 'react-navigation'
-import { ListItem } from 'react-native-elements'
+import { ListItem, Overlay, Button, Text, Input } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/Entypo'
+
+class PlusSign extends Component {
+  _addStudent() {
+    alert("not implemented")
+  }
+
+  render() {
+    return (
+      <View style={{ flexDirection: 'row' }}>
+        <Icon name='plus' color='white' size={30} onPress={this._addStudent.bind(this)} />
+      </View>
+    );
+  }
+}
 
 export default class RosterScreen extends Component {
+
+  static navigationOptions = ({ navigation }) => ({
+    headerRight: () =>(<PlusSign navigationProps={navigation} />) ,
+  })
+
+  constructor(props) {
+    super(props);
+
+    this.state = {isOverlayVisible: false, newStudentName: ""};
+
+    this._startAddStudent = this._startAddStudent.bind(this);
+    this._endAddStudent = this._endAddStudent.bind(this);
+    this._handleName = this._handleName.bind(this);
+  }
 
   renderItem = ({ item }) => (
     <ListItem
@@ -23,23 +52,59 @@ export default class RosterScreen extends Component {
     {
       name: 'Amy Farha',
       avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-      subtitle: 'Vice President'
     },
     {
       name: 'Chris Jackson',
       avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-      subtitle: 'Vice Chairman'
     },
   ]
   
+  _startAddStudent() {
+    this.setState({isOverlayVisible: true});
+  }
   
+  _endAddStudent() {
+    this.setState({isOverlayVisible: false});
+    this.list.push({
+      name: this.state.newStudentName,
+      avatar_url: 'https://vignette.wikia.nocookie.net/spongebob/images/d/d7/SpongeBob_stock_art.png/revision/latest?cb=20190921125147',
+    })
+  }
+
+  _handleName = (name) => {
+    this.setState({newStudentName: name})
+  }
+
   render () {
     return (
-      <FlatList
-        keyExtractor={this.keyExtractor}
-        data={this.list}
-        renderItem={this.renderItem}
-      />
+      <View>
+        <Button title="Add Student" onPress={this._startAddStudent}/>
+        <FlatList
+          keyExtractor={this.keyExtractor}
+          data={this.list}
+          renderItem={this.renderItem}
+        />
+        <Overlay isVisible={this.state.isOverlayVisible}>
+          <Text h2>Add a Student</Text>
+          <View style={{flex: 1, flexDirection: "column",justifyContent:"space-between"}}>
+          <View>
+            <Input 
+              label="Name"
+              placeholder="Student Name"
+              onChangeText={this._handleName}
+              />
+          </View>
+          <View>
+          <Button 
+            title="Submit"
+            icon={
+              <Icon name="check" color="white" size={15}/>
+            }
+            onPress={this._endAddStudent}/>
+          </View>
+         </View>
+        </Overlay>
+      </View>
     )
   }
     

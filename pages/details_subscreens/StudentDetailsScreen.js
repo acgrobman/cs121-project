765101cef2a34c564eefdca6cddd2a874b6ad541
Adam Grobman <25434173+acgrobman@users.noreply.github.com>
2019-11-24 15:14:52 -0800

    Add  unconnected placeholder  student details page

---
+++ pages/details_subscreens/StudentDetailsScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View } from "react-native";
+import { Text } from 'react-native-elements'
+
+/** Displays details on a specific student. */
+export default class StudentDetailsScreen extends Component {
+
+    /** Render component on screen */
+    render() {
+        return (
+            <View>
+                <Text>Placeholder</Text>
+            </View>
+        )
+    }
+}
